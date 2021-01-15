b873865cababea390171278b1acdfa933ba59585
Seyram <alvinofori@expresspaygh.com>
2021-01-15 15:18:48 +0000

    improvement: implement fields structure with sequelize

--- models/fields.js
+++ models/fields.js
@@ -1,5 +1,26 @@
+const { db } = require('./index');
+const { DataTypes } = require('sequelize');
+
 /**
  * id: uuid, defaultValue: uuidv4
  * label: string
  * isAqivahField: boolean, defaultValue: false
  */
+
+const Fields = db.define('Fields', {
+  id: {
+    type: DataTypes.UUID,
+    primaryKey: true,
+    defaultValue: DataTypes.UUIDV4,
+  },
+  label: {
+    type: DataTypes.STRING,
+
+  },
+  isAqivahField: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  }
+});
+
+export default Fields;
