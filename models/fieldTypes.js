8a0bfdaf1fbc7616cfda4fab79c5acacfb098792
Seyram <alvinofori@expresspaygh.com>
2021-01-15 15:21:22 +0000

    improvement: implement fieldTypes table definition

--- models/fieldTypes.js
+++ models/fieldTypes.js
@@ -1,4 +1,20 @@
+const { db } = require('./index');
+const { DataTypes } = require('sequelize');
 /**
  * id: uuid, defaultValue: uuidv4
  * label: string
  */
+
+const FieldType = db.define('FieldType', {
+  id: {
+    type: DataTypes.UUID,
+    primaryKey: true,
+    defaultValue: DataTypes.UUIDV4,
+  },
+  label: {
+    type: DataTypes.STRING,
+
+  }
+});
+
+export default FieldType;
