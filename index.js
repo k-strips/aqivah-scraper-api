bf617204bbbc27b13e620c33d5e8df1e21ec05ee
Seyram <alvinofori@expresspaygh.com>
2021-01-28 23:48:33 +0000

    wip

--- index.js
+++ index.js
@@ -4,23 +4,13 @@ const cors = require('express-cors');
 // const { sequelize } = require('./__models');
 const express = require('express');
 const db = require('./models');
-// const db = new sqlite3.Database(
-//   './db/data.db',
-//   (error) => {
-//     if (error) return console.log('Failed to connect to database', error);
-//     console.log('Connected to database');
-//   });
-
 
 
 const sourceRoutes = require('./routes/sources');
 const fieldRoutes = require('./routes/fields');
-const scraperRoutes = require('./routes/scrapers');
-// const paginationTypeRoutes = require('./routes/paginationTypes');
 const propertyRoutes = require('./routes/properties');
 const fieldTypeRoutes = require('./routes/fieldTypes');
 const scrapingSessionRoutes = require('./routes/scrapingSessions');
-// const responser = require('@zarcobox/responser');
 
 const app = express();
 const PORT = 5000;
@@ -41,8 +31,6 @@ app.use((err, req, res, next) => {
 
 app.use('/sources', sourceRoutes);
 app.use('/fields', fieldRoutes);
-app.use('/scrapers', scraperRoutes);
-// app.use('/pagination-types', paginationTypeRoutes);
 app.use('/properties', propertyRoutes);
 app.use('/field-types', fieldTypeRoutes);
 app.use('/scraper-sessions', scrapingSessionRoutes);
@@ -67,7 +55,3 @@ app.listen(PORT, async () => {
 // module.exports.db = db;
 module.exports.db = db;
 
-// exports.db = db;
-// exports.getDb = getDb;
-
-// module.exports = { db, getDb };
