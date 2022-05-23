f1baf1300f6952e8ad35b568a7d45f8c48e44aa9
k-strips <henblackstep@gmail.com>
2022-05-23 15:59:49 +0000

    [UPDATE] changed connection instance

--- models/index.js
+++ models/index.js
@@ -8,23 +8,26 @@ const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
+// let sequelize;
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(
+//     config.database,
+//     config.username,
+//     config.password,
+//     config
+//   );
+// }
 
 console.log(config);
 
-// // const sequelize = new Sequelize(
-// //   "postgres://stephen:qwerty_1@localhost:5432/accertix?schema=scraper"
-// // );
+const sequelize = new Sequelize(config.DATABASE_URI, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: true,
+  },
+});
 
 fs.readdirSync(__dirname)
   .filter((file) => {
